9fdd1e67d1fcaaee4a72de208130a2355d7efeba
lucifer-morningstar <slobeck@gmail.com>
2025-12-07 19:45:32 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -2,7 +2,7 @@
 tmux display -p "get-cur-buffer called"
 DEBUG=$1
 main() {
-  local SOCKS="/run/user/1000"
+  local SOCKS="/tmp/"
   local PANE_PID="$(tmux display -p "#{pane_pid}")"
   local SOCKET="$(ls "${SOCKS}" | grep "${PANE_PID}")"
   #local BUF_NAME="$( nvim --server "${SOCKET}" --remote-expr 'bufname("%:t")' )"
