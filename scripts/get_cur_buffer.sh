6cefed6c53ba1870d2d116d85e47ac9ba9a4a125
lucifer-morningstar <slobeck@gmail.com>
2025-12-09 06:56:57 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -4,11 +4,17 @@ DEBUG=$1
 main() {
   local PANE_PID="$(tmux display -p "#{pane_pid}")"
   local SOCKET="/tmp/$(ls /tmp | grep -E "${PANE_PID}")"
+  local PROC="$(ps -ppid "${PANE_PID}" | awk '{print $1)')"
   local BUF_NAME="$( nvim --server ${SOCKET} --remote-expr 'expand("%:t")' )"
   if (( $DEBUG == 1 )); then 
     debug "SOCKET: ${SOCKET}"
+    debug "PROC: ${PROC}"
     [[ -n "${BUF_NAME}" ]] && debug "BUF_NAME: ${BUF_NAME}" || fatal "bufname not found."  
   fi
+}
+
+set_status() {
+
 }
 
 debug() {
