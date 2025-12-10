5f06ed07a6adb35cf398c829b22dbe8778191e8b
lucifer-morningstar <slobeck@gmail.com>
2025-12-10 15:55:37 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -37,6 +37,7 @@ main() {
 
 dummy() {
   echo "to the void" > /dev/null
+  echo "this is dumb"
 }
 
 set_status() {
@@ -49,8 +50,8 @@ set_status() {
 
 debug() {
   [[ "$1" ]] && local OUT="${1}" || local OUT="no data"
-  local FMT='%10s | %-56s'
-  printf "${FMT}\n" "${OUT%:*}" "${OUT#*:}"
+#  local FMT='%10s | %-56s'
+#  printf "${FMT}\n" "${OUT%:*}" "${OUT#*:}"
 }
 
 fatal() {
