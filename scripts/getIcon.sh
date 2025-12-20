22911b726b7763be15067357110e8821fe503636
lucifer-morningstar <slobeck@gmail.com>
2025-12-20 04:20:59 -0800

    basic push. code/fizes

--- scripts/getIcon.sh
+++ scripts/getIcon.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env bash
 local icons="$(tmux display "#{@LIB_ICON}" )"
-tmux display "#{@LIB_ICON}"
-local yqbin="/usr/bin/yq"
-if ! command -v "${}" &> /dev/null; then
-  fatal "yq executable not found at ${yqbin}."
-fi
-main() {
-  local iconp="$1"
-  printf 'icons: %s\n' "$iconp" 
-
-}
-
-fatal() {
-  [[ -n "$1" ]] && local E="unknown error" || local E="$1"
-  printf 'ERROR: %s\n' "$E" >&2
-  exit 1
-}
-
-main $icons
+printf 'icons: %s', "test-string"
