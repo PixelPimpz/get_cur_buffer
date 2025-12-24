1e07f1ce614ca437056cd28a79fe2d6fa32883bf
lucifer-morningstar <slobeck@gmail.com>
2025-12-23 23:39:09 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g session-created[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g session-window-changed[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g after-new-window[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g after-new-session[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g sessions-changed[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g after-refresh-client[11] 'display -p "${TMUX_ROOT}/tmux.conf reloaded'
+#!/usr/bin/env b
+tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/get_cur_buffer'
+tmux set-hook -g session-created[0] 'run "#{@PLUG_ROOT}/get_cur_buffer'
+tmux set-hook -g session-window-changed[0] 'run "#{@PLUG_ROOT}/get_cur_buffer'
+tmux set-hook -g after-new-window[1] 'run "#{@PLUG_ROOT}/get_cur_buffer'
+tmux set-hook -g after-new-session[1] 'run "#{@PLUG_ROOT}/get_cur_buffer'
+tmux set-hook -g sessions-changed[1] 'run "#{@PLUG_ROOT}/get_cur_buffer'
+tmux set-hook -g after-refr-client[11] 'display -p "${TMUX_ROOT}/tmux.conf reloaded'
