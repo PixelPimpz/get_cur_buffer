79fb690799a3c8a5feb71991fd8b68450e80deb1
lucifer-morningstar <slobeck@gmail.com>
2025-12-31 12:41:31 -0800

    Generic commit

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/get_cur_buffer' 
 tmux set-hook -g pane-exited[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
-tmux set-hook -g session-created[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
-tmux set-hook -g session-window-changed[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
-tmux set-hook -g after-new-window[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
-tmux set-hook -g after-new-session[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
