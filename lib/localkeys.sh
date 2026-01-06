06e578574a8ac34befd0c592c2d93a786555677a
lucifer-morningstar <slobeck@gmail.com>
2026-01-06 15:03:22 -0800

    Generic commit

--- lib/localkeys.sh
+++ lib/localkeys.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+for keybind in "$( tmux list-keys | grep 'next-window' | grep -iv 'wheel' )"; do
+  echo "$keybind" | awk '{printf "bind-key %-4s %s \\; %s\n", $4, $5, "added-command" }' >> ~/temp-tmux.log
+done
+
 echo "date +%l:%M:%S localkeys..." >> ~/temp-tmux.log
 tmux display  "localkeys.sh running..."
