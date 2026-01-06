b46e2b8ad1c139aa536babc72bfa4a8b88555917
lucifer-morningstar <slobeck@gmail.com>
2026-01-06 15:52:44 -0800

    Generic commit

--- scripts/localkeys.sh
+++ scripts/localkeys.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+outfile="$1"
 main {
   for keybind in "$( tmux list-keys | grep 'next-window' | grep -iv 'wheel' )"; do
-    echo "$keybind" | awk '{printf "bind-key %-4s %s \\; %s\n", $4, $5, "source-file ~/.tmux.conf" }' >> ~/temp-tmux.log
+    echo "$keybind" | awk '{printf "bind-key %-4s %s \\; %s\n", $4, $5, "source-file ~/.tmux.conf" }' >> $outfile 
   done
 }
 
