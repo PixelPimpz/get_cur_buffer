3c71e79a4a32421ced81744692ff966f6e26475a
lucifer-morningstar <slobeck@gmail.com>
2026-01-17 00:18:37 -0800

    Generic commit

--- scripts/tmux-nvim-info.sh
+++ scripts/tmux-nvim-info.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 ICONS=$( tmux display -p "#{@LIB_ICON}" )
-YQBIN='/usr/bin/yq'
-PLUG_ROOT=$( tmux display -p "#{@PLUG_ROOT}" )
-if ! command -v "${YQBIN}" &> /dev/null; then
-  fatal "yq executable not found at ${YQBIN}."
+LOCAL_ROOT=$( tmux display -p "#{@LOCAL_ROOT}" )
+SHARE="$( tmux show -gqv @CHER )"
+
+## source helper functions from @CHER
+source "$SHARE/dump.fun"
+source "$SHARE/fatal.fun"
+
+## check for yq
+YQ='/usr/bin/yq'
+if ! command -v "${YQ}" &> /dev/null; then
+  fatal "yq executable not found at ${YQ}."
 fi
 
-main() {
+main() 
+{
   local PANE_PID="$(tmux display -p "#{pane_pid}")"
   local SOCKET="/tmp/$(ls /tmp | grep -E "${PANE_PID}")"
   local CHILD_PROC="$(ps -o comm= --ppid "${PANE_PID}")"
@@ -18,46 +26,31 @@ main() {
     local BUF_NAME="$( nvim --server ${SOCKET} --remote-expr 'expand("%:t")' )"
     local STATUS="${ICON} ${BUF_NAME}"
   else
-    local ICON="$("${YQBIN}" ".icons.apps.${PARENT_PROC}" "${ICONS}")"
+    local ICON="$("${YQ}" ".icons.apps.${PARENT_PROC}" "${ICONS}")"
     local EXIT=$? && (( ${EXIT} != 0 )) && fatal "yq failed with code ${EXIT}. Check yaml for path & syntax."
     local BUF_NAME="${PARENT_PROC}"
     SOCKET="none"
   fi
 
-  debug "PLUG_ROOT:$PLUG_ROOT"
-  debug "PANE_PID:${PANE_PID}"
-  debug "SOCKET:${SOCKET}"
-  debug "CHILD_PROC:${PARENT_PROC}"
-  debug "PARENT_PROC:${PARENT_PROC}"
-  debug "ICONS:~/${ICONS#*/home*$USER/}"
-  debug "ICON:${ICON}"
-  debug "STATUS:${STATUS}"
+  dump "LOCAL_ROOT:$LOCAL_ROOT"
+  dump "PANE_PID:${PANE_PID}"
+  dump "SOCKET:${SOCKET}"
+  dump "CHILD_PROC:${PARENT_PROC}"
+  dump "PARENT_PROC:${PARENT_PROC}"
+  dump "ICONS:~/${ICONS#*/home*$USER/}"
+  dump "ICON:${ICON}"
+  dump "STATUS:${STATUS}"
 
   ## set status bar 
   set_status "${ICON} ${BUF_NAME}"
 }
 
-set_status() {
+set_status() 
+{
   local STATUS="$1"
-  tmux set -g @unit-style "fg=#{@Light2},bg=#{@Dark2}"
-  tmux set -g @unit-style-end "fg=#{@Dark2},bg=#{@Dark0}"
-  tmux set -g @CurrentData "${STATUS}"
-  tmux set -g status-right-length 0
-  tmux set -g status-right "#[#{E:@unit-style-end}]#{@TriangleL}#[#{E:@unit-style}] #{@CurrentData} #[#{E:@unit-style-end}]#{@TriangleRInverse}#{T:@DateTime}"
-}
-
-debug() {
-  if (( $(tmux show -gqv @DEBUG) == 1 )); then
-    [[ "$1" ]] && local OUT="${1}" || local OUT="no data"
-    local FMT=' %10s | %-56s '
-    printf "${FMT}\n" "${OUT%:*}" "${OUT#*:}"
-  fi
-}
-
-fatal() {
-  [[ -n "$1" ]] && local E="unknown error" || local E="$1"
-  printf 'ERROR: %s\n' "$E" >&2
-  exit 1
+  tmux set -g @nvim-info-unit-style "fg=#{@Light2},bg=#{@Dark2}"
+  tmux set -g @nvim-info-unit-style-end "fg=#{@Dark2},bg=#{@Dark0}"
+  tmux set -g @nvim-info-unit "#[#{E:@unit-style-end}]#{@TriangleL}#[#{E:@unit-style}] ${STATUS} #[#{E:@unit-style-end}]#{@TriangleRInverse}"
 }
 
 main
--- tmux-nvim-info.tmux
+++ tmux-nvim-info.tmux
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux bind-key T run-shell "$CURRENT_DIR/scripts/tmux-nvim-info.sh"
+LOCAL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+tmux bind-key T run-shell "$LOCAL_ROOT/scripts/tmux-nvim-info.sh"
 
 tmux setenv "@LIB_ICON" "$TMUX_ROOT/lib/app-icons.yaml"
-tmux set -g '@PLUG_ROOT' "$CURRENT_DIR"
-tmux run-shell "$CURRENT_DIR/scripts/localkeys.sh" 
-tmux run-shell "$CURRENT_DIR/scripts/localhooks.sh" 
-tmux run-shell "$CURRENT_DIR/scripts/tmux-nvim-info.sh" 
-tmux set -u '@PLUG_ROOT' 
+tmux set -g '@LOCAL_ROOT' "$LOCAL_ROOT"
+tmux run-shell "$LOCAL_ROOT/scripts/localkeys.sh" 
+tmux run-shell "$LOCAL_ROOT/scripts/localhooks.sh" 
+tmux run-shell "$LOCAL_ROOT/scripts/tmux-nvim-info.sh" 
